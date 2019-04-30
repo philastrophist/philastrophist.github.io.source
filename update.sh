ca55db82f6e4007f6383cf15a1a43f7cf902d767
Shaun C Read <shaun.c.read@gmail.com>
2019-04-30 10:34:02 +0100

    test

---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+python generator/update.py
+
+# if git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'latex/' | grep -v "*.pdf"
+# then
+sudo apt-get install texlive-xetex
+tlmgr init-usertree
+sudo tlmgr update --all
+install fontawesome
+
+# Install tectonic using conda
+# wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+# bash miniconda.sh -b -p $HOME/miniconda
+# export PATH="$HOME/miniconda/bin:$PATH"
+# hash -r
+# conda config --set always_yes yes --set changeps1 no
+# conda update -q conda
+# conda info -a
+# conda create --yes -n latex
+# source activate latex
+# conda install -c conda-forge -c pkgw-forge tectonic
+
+# Build the paper using tectonic
+cd latex
+# tectonic cv-shauncread.tex --print
+cp cv-shauncread.pdf cv-shauncread.pdf.bkp
+xelatex  -interaction nonstopmode cv-shauncread.tex
+
+# Force push the paper to GitHub
+cd $TRAVIS_BUILD_DIR
+git checkout --orphan $TRAVIS_BRANCH-pdf
+git rm -rf .
+git add -f latex/cv-shauncread.pdf
+git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
+git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
+# fi
