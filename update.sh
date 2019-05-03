abc6fff80b2682f8fb0153eadb70208155df339e
Shaun C Read <shaun.c.read@gmail.com>
2019-05-03 13:09:34 +0100

    autobuild test

--- update.sh
+++ update.sh
@@ -1,31 +1,53 @@
 #!/bin/bash -x
+# update the site from UPDATE_BRANCH. Adds a new commit to LIVE_BRANCH after every new commit on UPDATE_BRANCH.
+# set a cron job to run on LIVE_BRANCH
 set -e
 
-UPDATE_BRANCH="autocompile"
-LIVE_BRANCH="test"
+UPDATE_BRANCH="test-develop"
+LIVE_BRANCH="test-master"
 
+git flow init -d
+git config gitflow.branch.develop $UPDATE_BRANCH
+git config gitflow.branch.master $LIVE_BRANCH
+git config gitflow.prefix.hotfix cron
 
-if [[ $TRAVIS_BRANCH  == $UPDATE_BRANCH ]]; then
-
-	wget https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb
-	sudo dpkg -i pandoc-2.7.2-1-amd64.deb
-	pip3 install pypandoc pyyaml requests ads
-	python3 generator/update.py
-
-	# if git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'latex/' | grep -v "*.pdf"; then  # if pdf needs generating
-	# sudo tlmgr init-usertree
-	# sudo tlmgr update --all
-	# sudo tlmgr install fontawesome
-	cd latex
-	../latexdockercmd.sh xelatex -interaction nonstopmode cv-shauncread.tex
-
-	# fi
-
-	# Force push to GitHub
-	cd ..
-	git checkout $LIVE_BRANCH || git checkout -b $LIVE_BRANCH
-  git add _publications/ latex/ files/
-	git -c user.name='travis' -c user.email='travis' commit -m "updating live version"
-	git push https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $LIVE_BRANCH
-
+TYPE='INVALID'
+if [[ $TRAVIS_EVENT_TYPE == 'cron' && $TRAVIS_BRANCH == "$LIVE_BRANCH" ]]; then
+  TYPE='CRON'
+elif [[ $TRAVIS_BRANCH == "$UPDATE_BRANCH" ]]; then
+  TYPE='USER'
 fi
+
+if [[ ($TYPE == 'CRON') || ( $TYPE == 'USER' ) ]]; then
+  wget https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb
+  sudo dpkg -i pandoc-2.7.2-1-amd64.deb
+  pip3 install pypandoc pyyaml requests ads
+  python3 generator/update.py
+
+  cd latex
+  ../latexdockercmd.sh xelatex -interaction nonstopmode cv-shauncread.tex
+
+  # Push to GitHub
+  cd ..
+
+  if [[ $TYPE == 'CRON' ]]; then
+    git flow hotfix start travis
+  elif [[ $TYPE == 'USER' ]]; then
+    git flow release start travis
+  fi
+
+  git add _publications/ latex/ files/
+  git -c user.name='travis' -c user.email='travis' commit -m "updating live version"
+
+
+  if [[ $TYPE == 'CRON' ]]; then
+    git flow hotfix finish travis
+  elif [[ $TYPE == 'USER' ]]; then
+    git flow release finish travis
+  fi
+
+
+  git push https://"$GITHUB_USER":"$GITHUB_API_KEY"@github.com/"$TRAVIS_REPO_SLUG" $UPDATE_BRANCH
+  git push https://"$GITHUB_USER":"$GITHUB_API_KEY"@github.com/"$TRAVIS_REPO_SLUG" $LIVE_BRANCH
+fi
+  
