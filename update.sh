f2cea4b4a4e1e78b8085245581209377f88ff138
Shaun Read <shaun.c.read@gmail.com>
2019-05-13 10:54:47 +0100

    move cv pdf to /cv.pdf

--- update.sh
+++ update.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -x
-# update the site from UPDATE_BRANCH. Adds a new commit to LIVE_BRANCH after every new commit on UPDATE_BRANCH.
-# set a cron job to run on LIVE_BRANCH
-set -e
-
-
-#! /bin/bash 
 set -e
 
 wget https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb
@@ -17,6 +11,7 @@ if git status --porcelain | grep latex/ | grep -v "*.pdf"; then
   # do latex stuff
   cd latex
   ../latexdockercmd.sh xelatex -interaction nonstopmode cv-shauncread.tex
+  cp cv-shauncread.pdf ../cv.pdf
   cd ..
 fi
 
